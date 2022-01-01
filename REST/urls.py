99d83ce16a9340717fcb24597a387603b2b81881
Lalluviadel <inspiracion@yandex.ru>
2022-01-01 18:35:41 +0300

    UPDHomework for Lesson_01: - added Authors app;

--- REST/urls.py
+++ REST/urls.py
@@ -2,12 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from users.views import UserModelViewSet
+from authors.views import AuthorModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('authors', AuthorModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 ]
