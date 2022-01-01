6f584848c19dff5738ebf4827e39629c3f5b7aad
Lalluviadel <inspiracion@yandex.ru>
2022-01-02 01:13:06 +0300

    Homework for Lesson_02: - added Users, Menu and Footer components; - created list of users page;

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../App.css';
+
+class Menu extends React.Component {
+    render() {
+        let links = [
+            'Home',
+            'Мой профиль',
+            'Мои проекты'
+        ]
+        return (<ul className={'navbar'}>
+            {links.map((value, index) => {
+                return (<li className={'navbar__item'} key={index}>
+                    <Link value={value}/>
+                </li>)
+            })}
+        </ul>
+        )}
+}
+
+class Link extends React.Component {
+    render() {
+        const url = '/' + this.props.value.toLowerCase().trim().replace(' ', '-')
+        return <div>
+            <a href={url}>
+                {this.props.value}
+            </a>
+        </div>
+    }
+}
+
+export default Menu
